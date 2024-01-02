b9696e28d3923ca8f0da8ab33f5ce8fcf55b305b
harrymaurya05 <harrymaurya05@gmail.com>
2024-01-02 09:37:43 +0530

    Explore To book component add on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 
 function App() {
   return (
+    <div>
     <Navbar/>
+    <ExploreTopBooks/>
+    </div>
   );
 }
 
