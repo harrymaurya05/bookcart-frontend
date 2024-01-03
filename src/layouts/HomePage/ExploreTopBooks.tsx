71606526a51c2628b5235562f6433d2f14e01076
harrymaurya05 <harrymaurya05@gmail.com>
2024-01-03 22:25:47 +0530

    titile change

--- src/layouts/HomePage/ExploreTopBooks.tsx
+++ src/layouts/HomePage/ExploreTopBooks.tsx
@@ -5,7 +5,7 @@ export const ExploreTopBooks = () =>{
                 <div>
                     <h1 className="display-5 fw-bold"> Find your next adventure</h1>
                     <p className="col-md-8 fs-4"> Where would you like to go next?</p>
-                    <a type="button" className="btn main-color btn-lg  text-while">Explore Top Books</a>
+                    <a type="button" className="btn main-color btn-lg  text-while">Explore Top My Books</a>
                 </div>
             </div>
 
