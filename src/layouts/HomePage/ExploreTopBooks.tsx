832232378cc120ba625170b8a5cb0e1c468ba116
harrymaurya05 <harrymaurya05@gmail.com>
2024-01-04 17:13:15 +0530

    text change

--- src/layouts/HomePage/ExploreTopBooks.tsx
+++ src/layouts/HomePage/ExploreTopBooks.tsx
@@ -3,7 +3,7 @@ export const ExploreTopBooks = () =>{
         <div className="p-5 mb-4 bg-dark header">
             <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
                 <div>
-                    <h1 className="display-5 fw-bold"> Find your next adventure</h1>
+                    <h1 className="display-5 fw-bold"> Find your next Trips</h1>
                     <p className="col-md-8 fs-4"> Where would you like to go next?</p>
                     <a type="button" className="btn main-color btn-lg  text-while">Explore Top My Books</a>
                 </div>
