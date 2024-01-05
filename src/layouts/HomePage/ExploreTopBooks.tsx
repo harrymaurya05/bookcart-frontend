44c9d3cb492e394f7376a59eafec71cd854a87a7
harrymauray05 <harrymaurya05@gmail.com>
2024-01-05 22:41:22 +0530

    text chnge

--- src/layouts/HomePage/ExploreTopBooks.tsx
+++ src/layouts/HomePage/ExploreTopBooks.tsx
@@ -4,7 +4,7 @@ export const ExploreTopBooks = () =>{
             <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
                 <div>
                     <h1 className="display-5 fw-bold"> Find your next Trips</h1>
-                    <p className="col-md-8 fs-4"> Where would you like to go next?</p>
+                    <p className="col-md-8 fs-4"> Where would you like to go next with me?</p>
                     <a type="button" className="btn main-color btn-lg  text-while">Explore Top My Books</a>
                 </div>
             </div>
