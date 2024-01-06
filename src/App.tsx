17be20bd37a1fe0978648f6c73cef4179ff4682c
harrymauray05 <harrymaurya05@gmail.com>
2024-01-06 10:15:10 +0530

    carousel section added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
     <Navbar/>
     <ExploreTopBooks/>
+    <Carousel/>
     </div>
   );
 }
