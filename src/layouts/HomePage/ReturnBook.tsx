ecf3c792a81ef48dd0f983e050f787aade85228b
harrymauray05 <harrymaurya05@gmail.com>
2024-01-23 19:23:22 +0530

    text vhange

--- src/layouts/HomePage/ReturnBook.tsx
+++ src/layouts/HomePage/ReturnBook.tsx
@@ -9,7 +9,7 @@ export const ReturnBook = () => {
                     height='233'
                     alt="book"
                 />
-                <h6 className='mt-2'>Books hariom</h6>
+                <h6 className='mt-2'>Books kiran</h6>
                 <p>Love life Code</p>
                 <a className='btn main-color text-white' href='#'>My Book</a>
             </div>
